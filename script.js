432ab78ef169b7ca7a063de9e9d62a8ea2172aaa
Brayden <brayden.ogorman@gmail.com>
2021-06-21 08:51:42 +1000

    Commented and cleaned up code a little

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 
 const form = document.querySelector('#form') 
-const newDiv = document.createElement('div')
 const result = document.querySelector('.result')
 const playlist = document.querySelector('.playlist')
+const newDiv = document.createElement('div')
 
 result.append(newDiv)
 
+
+function createNewDiv(data) {
+
+  // Deconstruct API JSON nesting
+  const {title, subtitle, images} = data.track
+    newDiv.innerHTML += `
+      <div class='result-content'>
+        <h4 class='title'>${title}</h4>
+        <h5 class='subtitle'>${subtitle}</h5>
+        <div>
+          <img src="${images.coverart}" alt="" class='cover-art'>
+        </div>
+        <button class='add'>Add</button>
+      </div>
+      `
+}
+
+// Event listener for search button to perform API request
 form.addEventListener('submit', function(event) {
   event.preventDefault()
   newDiv.innerHTML = ''
@@ -19,42 +37,35 @@ form.addEventListener('submit', function(event) {
   })
   .then(resp => resp.json())
   .then(data => data.tracks.hits.forEach(data => {
-    
-    const {title, subtitle, images} = data.track
-    
-    newDiv.innerHTML += `
-      <div class='result-content'>
-        <h4 class='title'>${title}</h4>
-        <h5 class='subtitle'>${subtitle}</h5>
-        <div>
-          <img src="${images.coverart}" alt="" class='cover-art'>
-        </div>
-        <button class='add'>Add</button>
-      </div>
-      `
-      
-      const addButton = document.querySelectorAll('.add')
-      addButton.forEach(button => 
-        button.addEventListener('click', event => {
-          let addToPlaylist = event.target.parentNode.cloneNode(true)
-          const newButton = document.createElement('button')
-          // overwrite 
-          addToPlaylist.children[3].remove()
-          // create new button
-          
-          // appendnew button
-          addToPlaylist.append(newButton)
-          addToPlaylist.children[3].innerText = 'Remove'
-          addToPlaylist.children[3].classList.add('remove')
-          playlist.append(addToPlaylist)
+    // Calls Create new div function
+    createNewDiv(data)
 
-          const removeButton = document.querySelectorAll('.remove')
-          removeButton.forEach(removeButton =>
-            removeButton.addEventListener('click', event =>{
-              event.target.parentNode.remove()
-            }))
-        })
-        )
+    // Creates Add button
+    const addButton = document.querySelectorAll('.add')
+
+    // For each Add button, add event listener
+    addButton.forEach(button => 
+      button.addEventListener('click', event => {
+        let addToPlaylist = event.target.parentNode.cloneNode(true)
+        const newButton = document.createElement('button')
+        // Remove previous add button
+        addToPlaylist.children[3].remove()
+
+        // create and append new button
+        addToPlaylist.append(newButton)
+        addToPlaylist.children[3].innerText = 'Remove'
+        addToPlaylist.children[3].classList.add('remove')
+        playlist.append(addToPlaylist)
+
+
+        // Event listener for remove button to remove track from playlist
+        const removeButton = document.querySelectorAll('.remove')
+        removeButton.forEach(removeButton =>
+          removeButton.addEventListener('click', event =>{
+            event.target.parentNode.remove()
+          }))
+      })
+      )
     })
   )
   .catch(err => console.error(err))
